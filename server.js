c05a5b84b7972f695c6cb2789805704a9501872c
Rafael Pereira <rpstos@gmail.com>
2017-09-03 20:58:51 -0300

    router 2

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ mongoose.connect(config.DB).then(
   );
 
 const app = express();
-app.use(express.static('wwwroot'));
+app.use(express.static('/'));
 app.use(bodyParser.json());
 app.use(cors());
 app.use('/items', itemRoutes);
