fede547fc58e28d439d99fcb20a4c2bfe10a78af
Rafael Pereira <rpstos@gmail.com>
2017-09-03 12:40:24 -0300

    connection db

--- config/DB.js
+++ config/DB.js
@@ -1,3 +1,3 @@
 module.exports = {
-    DB: 'mongodb://humany.documents.azure.com:443/'
+    DB: 'mongodb://humany:q9bUKGld1xfDpAO1UFG7s1u5zBGlW1zr9RfcqD9btkupj5l1qPZdTeKuEBalfCvrF0HfHL44DE72T2yZqJIazA==@humany.documents.azure.com:10255/?ssl=true'
   };
